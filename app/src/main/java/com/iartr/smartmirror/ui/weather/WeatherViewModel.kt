f091a5bc054eca46bdbe92ca2d7304345fe0b484
Popov Nikita <58226087+Inoskeee@users.noreply.github.com>
2022-06-13 13:55:28 +0500

    Some changes

--- app/src/main/java/com/iartr/smartmirror/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/iartr/smartmirror/ui/weather/WeatherViewModel.kt
@@ -1,16 +1,28 @@
 package com.iartr.smartmirror.ui.weather
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.android.volley.toolbox.Volley
 
 class WeatherViewModel: ViewModel() {
 
+
+    //Current Temp Values
     public val currentTemp = MutableLiveData<String>()
     public val currentCity = MutableLiveData<String>()
-    public val updateTime = MutableLiveData<String>()
+    public val currentTime = MutableLiveData<String>()
+    public val currentIcon = MutableLiveData<String>()
+    public val currentInfo = MutableLiveData<String>()
+    public val currentWindSpeed = MutableLiveData<String>()
+    public val currentHumidity = MutableLiveData<String>()
+
+    //Week Temp Values
+    public val avgTemp = MutableLiveData<String>()
+    public val avgTime = MutableLiveData<String>()
+    public val avgIcon = MutableLiveData<String>()
+    public val avgInfo = MutableLiveData<String>()
+    public val avgWindSpeed = MutableLiveData<String>()
+    public val avgHumidity = MutableLiveData<String>()
 
     public fun getCurrentWeather() : LiveData<String>{
         return currentTemp
@@ -20,6 +32,39 @@ class WeatherViewModel: ViewModel() {
         return currentCity
     }
     public fun getCurrentUpdate() : LiveData<String>{
-        return updateTime
+        return currentTime
+    }
+    public fun getCurrentIcon() : LiveData<String>{
+        return currentIcon
+    }
+    public fun getCurrentInfo() : LiveData<String>{
+        return currentInfo
+    }
+    public fun getCurrentWind() : LiveData<String>{
+        return currentWindSpeed
+    }
+    public fun getCurrentHumidity() : LiveData<String>{
+        return currentHumidity
+    }
+
+
+    public fun getAvgTemp() : LiveData<String>{
+        return avgTemp
+    }
+    public fun getAvgTime() : LiveData<String>{
+        return avgTime
+    }
+
+    public fun getAvgIcon() : LiveData<String>{
+        return avgIcon
+    }
+    public fun getAvgInfo() : LiveData<String>{
+        return avgInfo
+    }
+    public fun getAvgWind() : LiveData<String>{
+        return avgWindSpeed
+    }
+    public fun getAvgHumidity() : LiveData<String>{
+        return avgHumidity
     }
 }
