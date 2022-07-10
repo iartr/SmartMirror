0efbe988241440308a96f6091078897c13fb42c1
a.ilkaev <a.ilkaev@corp.vk.com>
2022-07-10 19:14:25 +0500

    ☁️ Feature: weather

--- features/weather/src/main/java/com/iartr/smartmirror/weather/WeatherNetworkDataSource.kt
+++ features/weather/src/main/java/com/iartr/smartmirror/weather/WeatherNetworkDataSource.kt
@@ -6,10 +6,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherNetworkDataSource {
-    @GET("weather?appid=$APP_ID&units=metric")
-    fun getCurrentWeatherByCoord(@Query("lat") lat: Double, @Query("lon") lon: Double): Single<Weather>
-
-    private companion object {
-        private const val APP_ID = "efda6346477a346c48be1683bf7dc361"
-    }
+    @GET("weather?&units=metric")
+    fun getCurrentWeatherByCoord(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") appId: String = BuildConfig.API_KEY_WEATHER
+    ): Single<Weather>
 }
